d38d8ac3cab89ea12511715b54bdfdc7967c0861
Mikhail Lazarau (Michaux) <mikhail.lazarau1@gmail.com>
2021-05-10 23:11:31 +0300

    Hometask 1

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,25 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    NSString *numberStr = [number stringValue];
+    if ([[numberStr substringToIndex:1]  isEqual: @"-"]) {
+        numberStr = [numberStr substringFromIndex:1];
+    }
+    NSInteger length = [numberStr length];
+    
+    NSMutableArray *arr = [[NSMutableArray alloc] init];
+    NSUInteger i = 0;
+    while (i < length) {
+        NSRange range = [numberStr rangeOfComposedCharacterSequenceAtIndex:i];
+        NSString *chStr = [numberStr substringWithRange:range];
+        [arr addObject:chStr];
+//        NSInteger intValue = [chStr integerValue];
+//        NSNumber *nsNumber = [NSNumber numberWithInt:intValue];
+//        [arr addObject:nsNumber];
+        i += range.length;
+    }
+    
+    return [[arr reverseObjectEnumerator] allObjects];
 }
 
 @end
